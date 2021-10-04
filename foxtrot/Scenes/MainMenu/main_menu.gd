1f467f574d1323d4e8a2573a4fabd4c054c3a7f5
Jonah Bui <55065339+Jonahbui@users.noreply.github.com>
2021-10-04 13:01:31 -0500

    Fixed canvas scaling in project settings. Implemented basic main menu with non-functional buttons (excluding exit button).

--- foxtrot/Scenes/MainMenu/main_menu.gd
+++ foxtrot/Scenes/MainMenu/main_menu.gd
@@ -5,3 +5,17 @@ func _ready():
   # Add the settings menu so that the player can configure their settings
   pass
 
+func _on_NewGameButton_pressed():
+  pass # Replace with function body.
+
+func _on_LoadGameButton_pressed():
+  pass # Replace with function body.
+
+func _on_SettingsButton_pressed():
+  pass # Replace with function body.
+
+func _on_ExitButton_pressed():
+  get_tree().quit()
+
+func _on_CreditsButton_pressed():
+  pass # Replace with function body.
